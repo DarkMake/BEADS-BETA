37c7d55cad3755dab545aba010af41e1e004eac6
DarkMake <84800362+DarkMake@users.noreply.github.com>
2022-03-28 12:18:22 -0500

    scr file

---
+++ index.ts
@@ -0,0 +1,50 @@
+import { Client } from 'discord.js';
+import config from './config';
+import helpCommand from './commands';
+
+const { intents, prefix, token } = config;
+
+const client = new Client({
+  intents,
+  presence: {
+    status: 'online',
+    activities: [{
+      name: `${prefix}help`,
+      type: 'LISTENING'
+    }]
+  }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as: ${client.user?.tag}`);
+});
+
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return;
+  
+  if (message.content.startsWith(prefix)) {
+    const args = message.content.slice(prefix.length).split(' ');
+    const command = args.shift();
+
+    switch(command) {
+      case 'ping':
+        const msg = await message.reply('Pinging...');
+        await msg.edit(`Pong! The round trip took ${Date.now() - msg.createdTimestamp}ms.`);
+        break;
+
+      case 'say':
+      case 'repeat':
+        if (args.length > 0) await message.channel.send(args.join(' '));
+        else await message.reply('You did not send a message to repeat, cancelling command.');
+        break;
+
+      case 'help':
+        const embed = helpCommand(message);
+        embed.setThumbnail(client.user!.displayAvatarURL());
+        await message.channel.send({ embeds: [embed] });
+        break;
+    }
+  }
+});
+
+client.login(token);
